cbfb89e630fa5589ecdfadd5c9022dca76c95882
liaoqinggang <liaoqinggang@p2w.net.cn>
2019-01-18 16:02:27 +0800

    变量名冲突

---
+++ c/duplicate_variable_name/main.c
@@ -0,0 +1,24 @@
+/****************************************
+   > File Name: main.c
+   > Author: lqinggang
+   > Email: 1944058861@qq.com
+   > Create Time: Fri 18 Jan 2019 03:53:42 PM CST
+****************************************/
+
+
+#include <stdio.h>
+
+
+int main(int argc,char *argv[])
+{
+    int repeat;
+    int *repeat;
+    printf("%d\n", repeat);
+
+    int repeat1;
+    int repeat1;
+    printf("%d\n", repeat1);
+
+
+    return (0);
+}
